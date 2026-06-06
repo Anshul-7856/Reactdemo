6cff16fd5f47b0fdcae44a13b7336f83845d8894
Anshul7856 <anshulsinghking65@gmail.com>
2026-06-06 15:50:58 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is my first React app automated and deployed using yaml file in Azure devops.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
